cb18a275c999ab3c5e3b399a5abc299426989494
MitGohil <mdgohil111@gmail.com>
2023-08-02 15:15:53 +0530

    Card prop

--- src/Classcompo/Childcompoforclass.jsx
+++ src/Classcompo/Childcompoforclass.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Component } from "react";
 
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBBtn
+} from 'mdb-react-ui-kit';
+
+
 class Childcompoforclass extends Component {
     render() {
         return (
             <>
                 <h2>Child compoforclass</h2>
                 <h3>{this.props.name}  {this.props.email}</h3> {/*This is the receiver to accept data from parent.*/}
-                <h4>{ this.props.email}</h4>
+                <h4>{this.props.email}</h4>
+
+                <MDBCard>
+                    <MDBCardImage src={this.props.imgSrc} position='top' alt='...' />
+                    <MDBCardBody>
+                        <MDBCardTitle>{this.props.title}</MDBCardTitle>
+                        <MDBCardText>
+                            Some quick example text to build on the card title and make up the bulk of the card's content. {this.props.kuchbhi}
+                        </MDBCardText>
+                        <MDBBtn href='#'>Button</MDBBtn>
+                    </MDBCardBody>
+                </MDBCard>
             </>
         );
     }
--- src/Classcompo/Classcompoprop.jsx
+++ src/Classcompo/Classcompoprop.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import Childcompoforclass from "./Childcompoforclass"; //Compelsory for props. Load child 
+import bike from "./05.jpg"
 
 
 
@@ -12,12 +13,34 @@ class Classcompoprop extends Component {
         return (
             <>
                 <h1>Classcompoprop</h1>
-                <Childcompoforclass name="Mitesh" email="mdgohil111@gmail.com" /> {/* Name is a provider for transfer data from parent to child.
-                                                        we take any name for providr except variable name 
+                <Childcompoforclass name="Mitesh" email="mdgohil111@gmail.com" />
+                {/* Name is a provider for transfer data from parent to child.
+                                                        we take any name for providr except variable name
                                                         Not transfer data from child to parent using props.
                                                         Transfer data from child to parent using HOC(Higher order component)(Last topic of class)*/}
 
+                <div className="row">
+                    <div className="col-3">
+                        <Childcompoforclass title="Props Title" imgSrc="https://mdbootstrap.com/img/new/standard/nature/184.webp" />
+                    </div>
+
+                    <div className="col-3">
+                        <Childcompoforclass title="Product" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA4M3ip3piTUTYow4hV-7fuXK5vNxaSsjPXeZKx5uxjg&s" />
+                    </div>
+
+                    <div className="col-3">
+                        <Childcompoforclass title="Other Product" imgSrc={bike} kuchbhi="Mitesh" />
+                    </div>
+
+                    <div className="col-3">
+                        {/* {JSON.stringify(process.env)} */}
+                        {/* <Childcompoforclass title="Other Product" imgSrc={`${process.env.PUBLIC_URL}/BMWBike.jpg`} kuchbhi="Gohil" /> */}
+                        <Childcompoforclass title="Other Product" imgSrc={`../CM-cmp/Bike.jpg`} kuchbhi="Jaymeet" />
+                    </div>
+                </div>
             </>
+
+
         );
     }
 }
