7712e99256aecc384d5c8c01dc1ed3542d3ebd31
MitGohil <mdgohil111@gmail.com>
2023-09-18 17:43:43 +0530

    Edit posted data in curd

--- src/Funtionalcompo/19Crud.jsx
+++ src/Funtionalcompo/19Crud.jsx
@@ -4,7 +4,10 @@ import { useState } from 'react'
 const Crud = () => {
     const [name, setName] = useState("")
     const [alldata, setAlldata] = useState([])
+    const [show, setShow] = useState(false)
+    const [edittext, setEdittext] = useState("")
 
+    // Add function
     const handleadd = () => {
         // console.log("handle add");
         // console.log(name);
@@ -16,20 +19,46 @@ const Crud = () => {
             setName("")
         }
     }
+   
+
+    // Edit function
+    const handleedit = (i) => {
+        setName(alldata[i])
+
+        //Click on edit, show update button
+        setShow(true)
+
+        // print edited data
+        setEdittext(i)
+    }
+
+    // Update function
+    const handleupdate = () => {
+        alldata.splice(edittext, 1, name)
+        setAlldata([...alldata])
+        setName("")
+        setShow(false)
+
+    }
     console.log(alldata);
     return (
         <>
             <div className='text-center' >
 
                 <input className='m-2 p-3' type="text" value={name} name="" id="" onChange={(e) => setName(e.target.value)} />
-                <button className='m-2 p-4 btn btn-success' onClick={handleadd}>Add-Item</button>
+                {!show ? <button className='m-2 p-4 btn btn-success' onClick={handleadd}>Add-Item</button> :
+                    <button className='m-2 p-4 btn btn-success' onClick={handleupdate}>Update-Item</button>}
             </div>
             {
-                alldata.map((data) =>
+                alldata.map((data, i) =>
                     <div className='text-center'>
-                        <h1 className='fw-bold fst-italic text-decoration-underline'>{data}</h1>
-                        <button className='m-1 btn btn-info'>Edit</button>
-                        <button className='m-1 btn btn-info'>Delete</button>
+                        <table className='text-center w-100 '>
+                            <tr>
+                                <td className='border'><h1 className='fw-bold fst-italic text-decoration-underline'>{data}</h1></td>
+                                <td className='border w-25  '><button className='m-1 btn btn-info' onClick={() => handleedit(i)}>Edit</button></td>
+                                <td className='border w-25  '><button className='m-1 btn btn-info'>Delete</button></td>
+                            </tr>
+                        </table>
                     </div>
 
                 )
